28aba84b93c460249f740528e3ffa21ad9641ad5
Roman <roma-gromov-1990@mail.ru>
2023-09-02 20:53:22 +0500

    add 1

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', "")
+answers[1] = prompt('Как ваше Фамилия?', "")
+answers[2] = prompt('Сколько вам лет?', "")
+
+document.write(answers)
+
+console.log('1')
+
+
+
+
